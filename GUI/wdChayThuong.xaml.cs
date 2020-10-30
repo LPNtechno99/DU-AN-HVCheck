fad1f3b6054c7687c99f6194fd4288fb40e27836
LPNtechno99 <lpntechno99@gmail.com>
2020-10-30 17:25:34 +0700

    Tai len lan dau tien

---
+++ GUI/wdChayThuong.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for wdChayThuong.xaml
+    /// </summary>
+    public partial class wdChayThuong : Window
+    {
+        public wdChayThuong()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnCaiDat_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("OK");
+        }
+    }
+}
